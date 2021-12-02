45b14cabeed47e82cba2fb5502a3872620ec80b1
Edvin Larsson <edvinpontuslarsson@gmail.com>
2021-12-02 16:10:50 +0100

    initial own chart handling

---
+++ source/js/initializer.js
@@ -0,0 +1,12 @@
+const dataObjects = [
+  {
+    label: 'Metric A',
+    dataArray: [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
+  },
+  {
+    label: 'Metric B',
+    dataArray: [1.1, 2.2, 3.3, 1.1, 2.2, 3.3, 1.1, 2.2, 3.3, 1.1, 2.2, 3.3],
+  },
+];
+
+displayChart(dataObjects);
