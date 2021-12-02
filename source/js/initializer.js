1c4a8f7c41dc5b896ac0e6049f6b91a6c9a97e6f
Edvin Larsson <edvinpontuslarsson@gmail.com>
2021-12-02 16:46:23 +0100

    creates randomized linear metrics

--- source/js/initializer.js
+++ source/js/initializer.js
@@ -1,12 +1,25 @@
-const dataObjects = [
-  {
-    label: 'Metric A',
-    dataArray: [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
-  },
-  {
-    label: 'Metric B',
-    dataArray: [1.1, 2.2, 3.3, 1.1, 2.2, 3.3, 1.1, 2.2, 3.3, 1.1, 2.2, 3.3],
-  },
-];
+const dataObjects = [];
+const letters = ['A', 'B', 'C', 'D', 'E'];
+
+for (let i = 0; i < 5; i++) {
+  const obj = {};
+
+  obj.label = letters[i];
+
+  const dataArray = [];
+
+  const random = Math.random() * 10;
+
+  for (let i = 0; i < 12; i++) {
+    const value = random + i;
+    dataArray.push(value);
+  }
+
+  obj.dataArray = dataArray;
+
+  dataObjects.push(obj);
+}
+
+console.log(dataObjects);
 
 displayChart(dataObjects);
